cda66ed703c1c7bac7c2879fa61f20ebaad2a660
Isroil <webtechuzbek@gmail.com>
2025-05-19 10:50:49 +0500

    Start project

---
+++ app.js
@@ -0,0 +1,15 @@
+// 4-misol
+// A = +prompt('A = sonini kiriting')
+// B = +prompt('B = sonini kiriting')
+// document.writeln(A>2 && B<=3)
+
+// 12-misol
+// A = +prompt('A = sonini kiriting')
+// B = +prompt('B = sonini kiriting')
+// C = +prompt('C = sonini kiriting')
+// document.writeln(A>0 && B>0 && C>0)
+
+// 28-misol
+x = +prompt('x = sonini kiriting')
+y = +prompt('y = sonini kiriting')
+document.writeln((x>0 && y>0) || (x<0 && y<0))
